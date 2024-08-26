4bb66a321266f36597fa54cd2a2aae64bf892e58
brendoncronos <brendongomes39@gmail.com>
2024-08-26 23:20:48 +0000

    COMMIT

--- index.js
+++ index.js
@@ -11,39 +11,39 @@ const connection = mysql.createConnection({
     user: 'root', // Seu nome de usuário do MySQL
     password: 'senhadifficil@123', // Sua senha do MySQL
     database: 'usersdb'
-  });
+});
 
   // Conectar ao banco de dados
 connection.connect((err) => {
     if (err) throw err;
 
     console.log('Conectado ao banco de dados MySQL');
-  });
-  
-  // Middleware para analisar corpos de solicitação
-  app.use(bodyParser.json());
-  
-  // Rotas CRUD
-  
-  // Criar usuário
-  app.post('/users', (req, res) => {
+});
+
+// Middleware para analisar corpos de solicitação
+app.use(bodyParser.json());
+
+// Rotas CRUD
+
+// Criar usuário
+app.post('/users', (req, res) => {
     const { name, email } = req.body;
 
     const INSERT_USER_QUERY = `INSERT INTO users (name, email) VALUES (?, ?)`;
-    
+
     connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
-      if (err) throw err;
+        if (err) throw err;
 
-      res.send('Usuário criado com sucesso');
+        res.send('Usuário criado com sucesso');
     });
-  });
-  
-  // Obter todos os usuários
-  app.get('/users', (req, res) => {
-    connection.query('SELECT * FROM users', (err, results) => {
-      if (err) throw err;
+});
 
-      res.json(results);
+// Obter todos os usuários
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM users', (err, results) => {
+        if (err) throw err;
+
+        res.json(results);
     });
 });
 
@@ -52,11 +52,12 @@ app.get('/users/:id', (req, res) => {
   const userId = req.params.id;
 
   const SELECT_USER_QUERY = `SELECT * FROM users WHERE id = ?`;
-  
-  connection.query(SELECT_USER_QUERY, [userId], (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
+
+    connection.query(SELECT_USER_QUERY, [userId], (err, results) => {
+        if (err) throw err;
+
+        res.json(results);
+    });
 });
 
 
